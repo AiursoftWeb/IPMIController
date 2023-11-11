0315c08dc615a7b9a3992a3b2441affe6bae67ee
AnduinXue <anduin@aiursoft.com>
2023-11-11 15:25:38 +0000

    Fix lint.

--- src/Aiursoft.IpmiController/MonitorHandler.cs
+++ src/Aiursoft.IpmiController/MonitorHandler.cs
@@ -3,7 +3,6 @@ using Aiursoft.CommandFramework.Framework;
 using Aiursoft.CommandFramework.Models;
 using Aiursoft.CommandFramework.Services;
 using Aiursoft.IpmiController.Models;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
